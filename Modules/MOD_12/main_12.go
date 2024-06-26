ed3f033fa4341887ed55b07974d14add0f5fe84a
Edw590 <40151365+Edw590@users.noreply.github.com>
2024-06-26 10:44:11 +0100

    Get distance and user location on User Locator

--- Modules/MOD_12/main_12.go
+++ Modules/MOD_12/main_12.go
@@ -24,6 +24,7 @@ package MOD_12
 import (
 	"ULComm/ULComm"
 	"Utils"
+	"Utils/UtilsSWA"
 	"log"
 )
 
@@ -33,6 +34,9 @@ const TIME_SLEEP_S int = 30
 
 type DeviceInfo struct {
 	Device_id string
+	Last_comm int64
+	Last_time_used int64
+	Distance int
 	Curr_location string
 }
 
@@ -67,6 +71,10 @@ func init() {realMain =
 			for _, device := range devices {
 				var device_info DeviceInfo = DeviceInfo{
 					Device_id: device.Device_id,
+					Last_comm: device.Last_comm,
+					Last_time_used: device.Last_time_used,
+					Distance: -1,
+					Curr_location: "unknown",
 				}
 
 				for _, wifi_net := range device.System_state.Connectivity_info.Wifi_networks {
@@ -75,12 +83,16 @@ func init() {realMain =
 							if location.Address != "" {
 								if wifi_net.BSSID == location.Address {
 									device_info.Curr_location = location.Location
+									device_info.Distance = UtilsSWA.GetAbstrDistanceRSSILOCRELATIVE(
+										UtilsSWA.GetRealDistanceRSSILOCRELATIVE(wifi_net.RSSI, UtilsSWA.DEFAULT_TX_POWER))
 
 									break
 								}
 							} else {
 								if wifi_net.SSID == location.Name {
 									device_info.Curr_location = location.Location
+									device_info.Distance = UtilsSWA.GetAbstrDistanceRSSILOCRELATIVE(
+										UtilsSWA.GetRealDistanceRSSILOCRELATIVE(wifi_net.RSSI, UtilsSWA.DEFAULT_TX_POWER))
 
 									break
 								}
@@ -94,12 +106,18 @@ func init() {realMain =
 							if location.Address != "" {
 								if bluetooth_device.Address == location.Address {
 									device_info.Curr_location = location.Location
+									device_info.Distance = UtilsSWA.GetAbstrDistanceRSSILOCRELATIVE(
+										UtilsSWA.GetRealDistanceRSSILOCRELATIVE(bluetooth_device.RSSI,
+											UtilsSWA.DEFAULT_TX_POWER))
 
 									break
 								}
 							} else {
 								if bluetooth_device.Name == location.Name {
 									device_info.Curr_location = location.Location
+									device_info.Distance = UtilsSWA.GetAbstrDistanceRSSILOCRELATIVE(
+										UtilsSWA.GetRealDistanceRSSILOCRELATIVE(bluetooth_device.RSSI,
+											UtilsSWA.DEFAULT_TX_POWER))
 
 									break
 								}
@@ -113,9 +131,38 @@ func init() {realMain =
 				device_infos = append(device_infos, device_info)
 			}
 
+			var most_recent_device DeviceInfo = getMostRecentlyUsedDevice(device_infos)
+			if most_recent_device.Device_id != "" {
+				log.Println("Current user location:", most_recent_device.Curr_location)
+			}
+
+			// TODO: If the location is unknown, check the next most recently used device. And check how long ago it was
+			//  used and see if the user may be using that device
+
+			// TODO: Also check the most recent device, how long ago it was used and see if the user may still be using
+			//  it or not. If it was 4 hours ago, may not be of much use
+
+			// TODO: For each location, check the max distance
+
+			// TODO: Also check if the location changed on some device. The user must be with it then, even if not using
+			//  it.
+
 			if Utils.WaitWithStopTIMEDATE(module_stop, TIME_SLEEP_S) {
 				return
 			}
 		}
 	}
+}
+
+func getMostRecentlyUsedDevice(devices []DeviceInfo) DeviceInfo {
+	var most_recent_device DeviceInfo
+	var most_recent_time int64 = 0
+	for _, device := range devices {
+		if device.Last_time_used > most_recent_time {
+			most_recent_device = device
+			most_recent_time = device.Last_time_used
+		}
+	}
+
+	return most_recent_device
 }
