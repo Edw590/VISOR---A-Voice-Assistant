9a7b3cd4c42a844a794820a5aee0fd17766dab76
Edw590 <40151365+Edw590@users.noreply.github.com>
2024-10-06 22:56:48 +0100

    Fix implementation of GetNextSpeechSentence()
    Also simplify it. No more repeated words on the beginning (or anywhere,
    anyway).

--- Libraries/GPTComm/GPTComm/Speech.go
+++ Libraries/GPTComm/GPTComm/Speech.go
@@ -29,8 +29,8 @@ import (
 
 var time_begin_ms_GL int64 = -1
 var curr_entry_time_ms_GL int64 = -1
-var curr_idx_GL int = 0
 var last_speech_GL string = ""
+var last_idx_begin_GL int = 0
 
 const END_ENTRY string = "[3234_END]"
 const ALL_DEVICES_ID string = "3234_ALL"
@@ -83,58 +83,22 @@ func GetNextSpeechSentence() string {
 	var sentence string = ""
 	for {
 		var entry *_Entry = getEntry(curr_entry_time_ms_GL, -1)
-		var text_split []string = strings.Split(entry.getText(), " ")
+		var text = entry.getText()
 
 		//log.Println("--------------------------")
-		if curr_idx_GL >= len(text_split) {
-			break
-		}
-		//log.Println("len(text_split):", len(text_split))
-		for i := curr_idx_GL; i < len(text_split); i++ {
-			var word string = text_split[i]
-			//log.Println("curr_idx:", i)
-			//log.Println("word:", word)
+		//log.Println("text: \"" + text + "\"")
 
-			if strings.Contains(word, END_ENTRY) {
-				// If the word contains END_ENTRY, remove it and add a period at the end in case there's not already
-				// one. Example: "peers[3234_END]" --> "peers.". Or "peers.[3234_END]" --> "peers.".
+		text = strings.Replace(text, END_ENTRY, ".", -1)
+		var dot_idx = strings.IndexAny(text[last_idx_begin_GL:], ".!?")
 
-				// Add one more to go out of bounds next time the function is called. Will make it break the loop
-				// instantly.
-				curr_idx_GL++
+		// If the last dot index is not found, it means that the sentence is not finished yet. So, we must wait for the
+		// next entry to be added to the text file.
+		if dot_idx != -1 {
+			sentence = text[last_idx_begin_GL : last_idx_begin_GL+ dot_idx + 1]
+			sentence = strings.Trim(sentence, " ")
 
-				// But if the word is END_ENTRY alone, just break the loop and return whatever there is - including
-				// nothing, which is taken care of below.
-				if word == END_ENTRY {
-					break
-				}
+			last_idx_begin_GL += dot_idx + 1
 
-				word = strings.Replace(word, END_ENTRY, "", -1)
-				if !strings.HasSuffix(word, ".") && !strings.HasSuffix(word, "!") && !strings.HasSuffix(word, "?") {
-					word += "."
-				}
-			} else if word != "" {
-				curr_idx_GL++
-			} else {
-				continue
-			}
-
-			if sentence == "" {
-				sentence = word
-			} else {
-				sentence += " " + word
-			}
-
-			if strings.HasSuffix(word, ".") || strings.HasSuffix(word, "!") || strings.HasSuffix(word, "?") {
-				//log.Println("sentence: \"" + sentence + "\"")
-				//log.Println("word: \"" + word + "\"")
-				sentence = strings.TrimSuffix(sentence, " ")
-
-				break
-			}
-		}
-
-		if strings.HasSuffix(sentence, ".") || strings.HasSuffix(sentence, "!") || strings.HasSuffix(sentence, "?") {
 			break
 		}
 
@@ -143,13 +107,17 @@ func GetNextSpeechSentence() string {
 
 	//log.Println("sentence: \"" + sentence + "\"")
 
-	if sentence == "" {
+	if sentence == "" || !strings.ContainsAny(sentence, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") {
 		sentence = END_ENTRY
 		curr_entry_time_ms_GL = -1
-		curr_idx_GL = 0
+		last_idx_begin_GL = 0
 	}
 
-	last_speech_GL += " " + sentence
+	if last_speech_GL == "" {
+		last_speech_GL = sentence
+	} else {
+		last_speech_GL += " " + sentence
+	}
 
 	return sentence
 }
