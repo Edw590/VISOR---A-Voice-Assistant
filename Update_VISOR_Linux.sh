1153b026c9ce22fc5ba859428dce303f76fe9220
Victor Espinoza-Guerra <thesilvervelvets@gmail.com>
2024-10-12 14:08:13 -0700

    Update Update_VISOR_Linux.sh
    Removed tangling code

--- Update_VISOR_Linux.sh
+++ Update_VISOR_Linux.sh
@@ -3,8 +3,6 @@
 git checkout -f main
 git reset --hard origin/main
 git pull origin main
-python./tools/download_models.py
-python./tools/update_rope.py
 
 echo
 echo --------Update completed--------
