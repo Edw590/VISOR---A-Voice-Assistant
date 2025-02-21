36368341df80e9653f03e5371ceab51a384a83f0
Edw590 <40151365+Edw590@users.noreply.github.com>
2025-02-21 15:25:04 +0000

    Attempt of stopping user settings resetting
    Now it will receive the settings from the server if, aside from them
    having changed, the user email is not set locally.

--- Libraries/SettingsSync/SettingsSync/UserSettings.go
+++ Libraries/SettingsSync/SettingsSync/UserSettings.go
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2023-2024 The V.I.S.O.R. authors
+ * Copyright 2023-2025 The V.I.S.O.R. authors
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -61,7 +61,7 @@ func SyncUserSettings() {
 					last_get_settings_when_s = time.Now().Unix()
 				}
 
-				if update_settings && remoteSettingsChanged() {
+				if Utils.User_settings_GL.General.User_email_addr == "" || update_settings && remoteSettingsChanged() {
 					if !Utils.IsCommunicatorConnectedSERVER() {
 						// Check again the communicator. Trying to prevent deadlocks.
 						continue
