f78fa202cb9c7409ae7924f82e3b015880a4d6da
Edw590 <40151365+Edw590@users.noreply.github.com>
2025-04-05 19:48:29 +0100

    Hide TOOL role from sessions history on Chats tab

--- ClientCode/Screens/ModGPTCommunicator.go
+++ ClientCode/Screens/ModGPTCommunicator.go
@@ -257,12 +257,12 @@ func gptCommunicatorCreateSessionsTab() *container.Scroll {
 
 								var msg_role = message_parts_slash[0]
 								switch msg_role {
-									case "system":
-										continue
 									case "assistant":
 										msg_role = "VISOR"
 									case "user":
 										msg_role = "YOU"
+									default:
+										continue
 								}
 
 								if len(message_parts_pipe) < 2 || message_parts_pipe[1] == "" {
@@ -275,7 +275,7 @@ func gptCommunicatorCreateSessionsTab() *container.Scroll {
 
 								msg_content_str +=
 									"-----------------------------------------------------------------------\n" +
-										"|" + strings.ToUpper(msg_role) + "| on " +
+										"|" + msg_role + "| on " +
 										Utils.GetDateTimeStrTIMEDATE(msg_timestamp_s) + ":\n" + msg_content + "\n\n"
 							}
 							if len(msg_content_str) > 2 {
